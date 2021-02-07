b7463f18d8fcbf930b1b07bf08c3ce7c4e47ccce
hipo760 <hipo760@outlook.com>
2021-02-07 21:27:51 +0800

    fix: expose logger configuration

--- Lumpy.Lib.Common/LoggerInstance.cs
+++ Lumpy.Lib.Common/LoggerInstance.cs
@@ -7,8 +7,8 @@ namespace Lumpy.Lib.Common
 {
     public static class LoggerInstance
     {
-        public static ILogger ConsoleLogger() => BuildConsoleLogger();
-        public static ILogger FileLogger(string logFilePath) => BuildFileLogger(logFilePath);
+        public static ILogger ConsoleLogger() => ConsoleLoggerConfiguration().CreateLogger();
+        public static ILogger FileLogger(string logFilePath) => FileLoggerConfiguration(logFilePath).CreateLogger();
         public static ILogger ConfiguredLogger(IConfiguration loggerConfiguration)
         {
             return loggerConfiguration == null
@@ -18,16 +18,17 @@ namespace Lumpy.Lib.Common
                     .Configuration(loggerConfiguration)
                     .CreateLogger();
         }
-        private static ILogger BuildConsoleLogger() =>
+
+        public static LoggerConfiguration ConsoleLoggerConfiguration() =>
             new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .Enrich.FromLogContext()
                 .Enrich.WithCaller()
                 .Enrich.WithThreadId().WriteTo.Async(a => a.Console(
                     LogEventLevel.Verbose,
-                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({ThreadId}) [{Caller}] {Message}{NewLine}{Exception}"))
-                .CreateLogger();
-        private static ILogger BuildFileLogger(string logFilePath) =>
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({ThreadId}) [{Caller}] {Message}{NewLine}{Exception}"));
+
+        public static LoggerConfiguration FileLoggerConfiguration(string logFilePath) =>
             string.IsNullOrEmpty(logFilePath) || string.IsNullOrWhiteSpace(logFilePath)
                 ? throw new ArgumentNullException(nameof(logFilePath))
                 : new LoggerConfiguration()
@@ -42,7 +43,9 @@ namespace Lumpy.Lib.Common
                             rollingInterval: RollingInterval.Day,
                             outputTemplate:
                             "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level}] ({ThreadId}) [{Caller}] {Message}{NewLine}{Exception}");
-                    })
-                    .CreateLogger();
+                    });
+
+
+
     }
 }
